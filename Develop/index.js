41aceff6988c084bd6f3c2051f624ad27e8ece01
Michael B <bosse730@gmail.com>
2023-01-26 22:52:24 +1100

    called inquirer and added question prompts

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,76 @@
-// TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const fs = require('fs')
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// questions which take in user input for readme
+const questions = [ 
+{
+    type: 'input',
+    message: 'Project title:',
+    name: 'title',
+},
+{
+    type: 'input',
+    message: 'Project description:',
+    name: 'description',
+},
+{
+    type: 'input',
+    message: 'Installation:',
+    name: 'installation',
+},
+{
+    type: 'input',
+    message: 'Usage:',
+    name: 'usage',
+},
+{
+    type: 'list',
+    message: 'License:',
+    name: 'license',
+    choices: ["No License", "Apache 2.0", "GNU General Public License v3.0", "MIT License", "BSD 2-Clause 'Simplified' License", "BSD 3-Clause 'New' or 'Revised' License"]
+},
+{
+    type: 'input',
+    message: 'Contributing:',
+    name: 'contributing',
+},
+{
+    type: 'input',
+    message: 'Tests:',
+    name: 'tests',
+},
+{
+    type: 'input',
+    message: 'Questions - GitHub Username:',
+    name: 'githubUsername',
+    
+},
+{
+    type: 'input',
+    message: 'Questions - Email Address:',
+    name: 'emailAddress',
+    
+}];
 
-// TODO: Create a function to initialize app
-function init() {}
+// this function writes a new readme file
+function writeReadMe(filename, data){
+    fs.writeFile(`${filename}`, data, (err) =>
+        err ? console.log(err) : console.log("successfully created README file!")
+    )
+}
+
+// initialises the prompts for user input
+function init(){
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            //generate markdown based on response
+            let md = generateMarkdown(response)
+            //create new readme file
+            writeReadMe(`${response.title}.md`, md)
+        })}
 
 // Function call to initialize app
 init();
